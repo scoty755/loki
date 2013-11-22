c44105e46555decde5e7544045235575e8717356
scoty <scoty755@gmail.com>
2013-11-23 01:45:25 +0900

    Add LG G2(Isai) KDDI MODEL

--- loki_patch.c
+++ loki_patch.c
@@ -235,6 +235,14 @@ struct target targets[] = {
 		.hdr = 0xf8d51c4,
 		.lg = 1,
 	},
+    {
+		.vendor = "KDDI",
+		.device = "LG G2",
+		.build = "HGL2210a",
+		.check_sigs = 0xf813458,
+		.hdr = 0xf8358a4,
+		.lg = 1,
+	},
 	{
 		.vendor = "KT or LGU",
 		.device = "LG G2",
