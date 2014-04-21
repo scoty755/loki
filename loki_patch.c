69de7c4d96a51acebf5d46573b4f9665f32dae55
scoty755 <scoty755@gmail.com>
2014-04-22 00:14:03 +0900

    Add LG Optimus LIFE

--- loki_patch.c
+++ loki_patch.c
@@ -309,6 +309,14 @@ struct target targets[] = {
 		.lg = 1,
 	},	
 	{
+		.vendor = "DoCoMo",
+		.device = "LG Optimus LIFE",
+		.build = "L02E20a",
+		.check_sigs = 0x88f109b4,
+		.hdr = 0x88f743f8,
+		.lg = 1,
+	},
+	{
 		.vendor = "International",
 		.device = "LG Optimus F5",
 		.build = "P87510e",
